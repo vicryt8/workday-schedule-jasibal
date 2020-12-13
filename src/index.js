e877672328a40c9a0a03a00dd4d7d6b9af36c0d3
josaye <j.m.asibal@hotmail.com>
2020-12-13 21:58:06 +1100

    formatting and event functions complete

--- src/index.js
+++ src/index.js
@@ -2,32 +2,82 @@
 const moment = require('moment');
 const startHour = 9;
 const dayDuration = 8;
-const scheduleMinutesIncrement = 60;
-var currentDayHeader = document.getElementById('currentDay');
-var scheduleBody = document.getElementById('timeblocks');
+const timeBlockIncrement = 60;
+const now = moment();
+var currentDayHeader = document.querySelector('#currentDay');
+var scheduleBody = document.querySelector('#timeblocks');
+var todaysEvents = [];
+var currentDayEventCode = moment().format('DD-MM-YYYY');
 function init() {
     currentDayHeader.innerText = moment().format('LLL');
     loadTimeBlocks();
+    formatTimeBlocks();
+    loadEvents();
 }
 function loadTimeBlocks() {
-    for (var i = 0; i <= (dayDuration * 60) / scheduleMinutesIncrement; i++) {
+    for (var i = 0; i <= (dayDuration * 60) / timeBlockIncrement; i++) {
         var timeBlockHour = moment().startOf('day');
         var newTimeBlock = document.createElement('div');
         var newTimeBlockHeader = document.createElement('span');
         var newTimeBlockTextArea = document.createElement('textarea');
         var newTimeBlockButton = document.createElement('button');
         var newTimeBlockSaveIcon = document.createElement('span');
-        newTimeBlock.classList.add('input-group');
+        newTimeBlock.classList.add('input-group', 'row');
         newTimeBlockHeader.classList.add('input-group-text');
         newTimeBlockTextArea.classList.add('form-control');
         newTimeBlockButton.classList.add('btn', 'btn-outline-secondary');
         newTimeBlockSaveIcon.classList.add('material-icons', 'md-light');
+        newTimeBlock.setAttribute('data-time', String(i));
         newTimeBlockButton.type = 'button';
-        newTimeBlockHeader.innerText = timeBlockHour.add(startHour * 60 + i * scheduleMinutesIncrement, 'm').format('hh:mm A');
+        newTimeBlockButton.addEventListener('click', (event) => saveEvent(event));
+        newTimeBlockHeader.innerText = timeBlockHour.add(startHour * 60 + i * timeBlockIncrement, 'm').format('hh:mm A');
         newTimeBlockSaveIcon.innerText = 'save';
         newTimeBlockButton.appendChild(newTimeBlockSaveIcon);
         newTimeBlock.append(newTimeBlockHeader, newTimeBlockTextArea, newTimeBlockButton);
         scheduleBody.appendChild(newTimeBlock);
     }
+}
+function formatTimeBlocks() {
+    var timeBlockRows = document.querySelectorAll('.row');
+    var timeBlockFormats = ['future', 'present', 'past'];
+    timeBlockRows.forEach((item) => {
+        var timeBlockHour = moment().startOf('day');
+        var hour = Number(item.getAttribute('data-time'));
+        timeBlockHour = timeBlockHour.add(startHour * 60 + hour * timeBlockIncrement, 'm');
+        var formatCondition = 0 + (timeBlockHour.isSame(now, 'hour') ? 1 : 0) + (timeBlockHour.isBefore(now, 'hour') ? 2 : 0);
+        item.children[1].classList.add('class', timeBlockFormats[formatCondition]);
+    });
+}
+function loadEvents() {
+    todaysEvents = JSON.parse(String(localStorage.getItem(currentDayEventCode)));
+    if (todaysEvents !== null) {
+        todaysEvents.forEach((item) => {
+            var incrementedIndex = item.index * (item.increment / timeBlockIncrement);
+            if (scheduleBody.children[incrementedIndex] !== undefined) {
+                scheduleBody.children[incrementedIndex].children[1].value = item.eventText;
+            }
+        });
+    }
+    else {
+        todaysEvents = [];
+    }
+}
+function saveEvent(event) {
+    var timeBlockBody = event.currentTarget.parentNode;
+    var timeBlockIndex = Number(timeBlockBody.dataset.time);
+    console.log(event.currentTarget);
+    var newEvent = {
+        id: String(timeBlockIncrement) + '-' + String(timeBlockIndex),
+        increment: timeBlockIncrement,
+        index: timeBlockIndex,
+        eventText: String(timeBlockBody.children[1].value),
+    };
+    todaysEvents = todaysEvents.filter((item) => {
+        return item.id !== newEvent.id;
+    });
+    console.log(todaysEvents);
+    todaysEvents.push(newEvent);
+    localStorage.setItem(currentDayEventCode, JSON.stringify(todaysEvents));
+    loadEvents();
 }
 init();
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
 const moment = require('moment');
 const startHour = 9;
 const dayDuration = 8;
-const scheduleMinutesIncrement = 60;
+const timeBlockIncrement = 60;
+const now = moment();
 
-var currentDayHeader = document.getElementById('currentDay') as HTMLElement;
-var scheduleBody = document.getElementById('timeblocks') as HTMLElement;
+var currentDayHeader = document.querySelector('#currentDay') as HTMLElement;
+var scheduleBody = document.querySelector('#timeblocks') as HTMLElement;
+var todaysEvents: timeBlockEvent[] = [];
+
+var currentDayEventCode = moment().format('DD-MM-YYYY');
+
+type timeBlockEvent = {
+	id: string;
+	increment: number;
+	index: number;
+	eventText: string;
+};
 
 function init(): void {
 	currentDayHeader.innerText = moment().format('LLL');
 	loadTimeBlocks();
+	formatTimeBlocks();
+	loadEvents();
 }
 
 function loadTimeBlocks(): void {
-	for (var i = 0; i <= (dayDuration * 60) / scheduleMinutesIncrement; i++) {
+	for (var i = 0; i <= (dayDuration * 60) / timeBlockIncrement; i++) {
 		var timeBlockHour = moment().startOf('day');
 		var newTimeBlock = document.createElement('div');
 		var newTimeBlockHeader = document.createElement('span');
@@ -20,20 +33,67 @@ function loadTimeBlocks(): void {
 		var newTimeBlockButton = document.createElement('button');
 		var newTimeBlockSaveIcon = document.createElement('span');
 
-		newTimeBlock.classList.add('input-group');
+		newTimeBlock.classList.add('input-group', 'row');
 		newTimeBlockHeader.classList.add('input-group-text');
 		newTimeBlockTextArea.classList.add('form-control');
 		newTimeBlockButton.classList.add('btn', 'btn-outline-secondary');
 		newTimeBlockSaveIcon.classList.add('material-icons', 'md-light');
 
+		newTimeBlock.setAttribute('data-time', String(i));
 		newTimeBlockButton.type = 'button';
-		newTimeBlockHeader.innerText = timeBlockHour.add(startHour * 60 + i * scheduleMinutesIncrement, 'm').format('hh:mm A');
+		newTimeBlockButton.addEventListener('click', (event) => saveEvent(event));
+		newTimeBlockHeader.innerText = timeBlockHour.add(startHour * 60 + i * timeBlockIncrement, 'm').format('hh:mm A');
 		newTimeBlockSaveIcon.innerText = 'save';
 
 		newTimeBlockButton.appendChild(newTimeBlockSaveIcon);
 		newTimeBlock.append(newTimeBlockHeader, newTimeBlockTextArea, newTimeBlockButton);
 		scheduleBody.appendChild(newTimeBlock);
 	}
+}
+
+function formatTimeBlocks(): void {
+	var timeBlockRows = document.querySelectorAll('.row');
+	var timeBlockFormats = ['future', 'present', 'past'];
+	timeBlockRows.forEach((item) => {
+		var timeBlockHour = moment().startOf('day');
+		var hour = Number(item.getAttribute('data-time'));
+		timeBlockHour = timeBlockHour.add(startHour * 60 + hour * timeBlockIncrement, 'm');
+		var formatCondition = 0 + (timeBlockHour.isSame(now, 'hour') ? 1 : 0) + (timeBlockHour.isBefore(now, 'hour') ? 2 : 0);
+		item.children[1].classList.add('class', timeBlockFormats[formatCondition]);
+	});
+}
+
+function loadEvents(): void {
+	todaysEvents = JSON.parse(String(localStorage.getItem(currentDayEventCode)));
+	if (todaysEvents !== null) {
+		todaysEvents.forEach((item) => {
+			var incrementedIndex = item.index * (item.increment / timeBlockIncrement);
+			if (scheduleBody.children[incrementedIndex] !== undefined) {
+				(scheduleBody.children[incrementedIndex].children[1] as HTMLTextAreaElement).value = item.eventText;
+			}
+		});
+	} else {
+		todaysEvents = [];
+	}
+}
+
+function saveEvent(event: MouseEvent): void {
+	var timeBlockBody = <HTMLElement>(<HTMLElement>event.currentTarget).parentNode;
+	var timeBlockIndex = Number(timeBlockBody.dataset.time);
+	console.log(event.currentTarget);
+	var newEvent = {
+		id: String(timeBlockIncrement) + '-' + String(timeBlockIndex),
+		increment: timeBlockIncrement,
+		index: timeBlockIndex,
+		eventText: String((timeBlockBody.children[1] as HTMLTextAreaElement).value),
+	};
+	todaysEvents = todaysEvents.filter((item) => {
+		return item.id !== newEvent.id;
+	});
+	console.log(todaysEvents);
+	todaysEvents.push(newEvent);
+	localStorage.setItem(currentDayEventCode, JSON.stringify(todaysEvents));
+	loadEvents();
 }
 
 init();
